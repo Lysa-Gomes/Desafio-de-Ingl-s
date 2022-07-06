281af88403b78fe152ccb4bec0cbe28328535c83
Lysa-Gomes <laysagomesmello9@gmail.com>
2022-07-06 14:54:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let comedy =[
+{title: 'Just Go With It'}, 
+{director:'Dennis Dugan'}, 
+{year: 2011}
+];
+console.log(comedy);
+
+let action = [
+{title:'Avengers: Infinity War'}, 
+{director:'Anthony Russo, Joe Russo'}, 
+{year:2018}
+];
+console.log(action);
+
+let fantasy =[
+{title:'Harry Potter:and the philosopher stone'}, 
+{director:'Chris Columbus'}, 
+{year:2001}
+];
+console.log(fantasy);
+
+let Movie = true
+
+if( Movie === true){
+ console.log(`MY FAVORIT MOVIE IS ${action[0].title}`)
+}
+else{
+    console.log(`NOT EVEN 5 STARS `)
+}
